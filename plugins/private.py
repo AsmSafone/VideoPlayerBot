45c0287d604c9d25c901708be81635d8e35eee6b
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2021-09-19 17:05:41 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -33,7 +33,7 @@ async def start(client, message):
             ],
             [
                 InlineKeyboardButton("📢 CHANNEL", url=f"https://t.me/{UPDATES_CHANNEL}"),
-                InlineKeyboardButton("SOURCE 📦", url=f"https://github.com/AsmSafone/VideoPlayerBot"),
+                InlineKeyboardButton("SOURCE 📦", url=f"https://github.com/TharukRenuja/VideoPlayerBot"),
             ],
             [
                 InlineKeyboardButton("🤖 ABOUT", callback_data="about"),
@@ -93,7 +93,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],
             [
                 InlineKeyboardButton("📢 CHANNEL", url=f"https://t.me/{UPDATES_CHANNEL}"),
-                InlineKeyboardButton("SOURCE 📦", url=f"https://github.com/AsmSafone/VideoPlayerBot"),
+                InlineKeyboardButton("SOURCE 📦", url=f"https://github.com/TharukRenuja/VideoPlayerBot"),
             ],
             [
                 InlineKeyboardButton("🤖 ABOUT", callback_data="about"),
