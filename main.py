e000f726e0808ac9ca43b9b2905b2d0cd8a97aa5
arullk5566 <85780324+arullk5566@users.noreply.github.com>
2021-09-29 17:25:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ if not os.path.isdir("./downloads"):
 
 Bot.start()
 User.start()
-print("\n[INFO] - STARTED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
+print("\n[INFO] - STARTED VIDEO PLAYER BOT, JOIN @klimanam !")
 
 idle()
 Bot.stop()
 User.stop()
-print("\n[INFO] - STOPPED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
+print("\n[INFO] - STOPPED VIDEO PLAYER BOT, JOIN @klimanam !")
