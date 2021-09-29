305c0a11e07697a14cbba1f0182944e0121cafb8
arullk5566 <85780324+arullk5566@users.noreply.github.com>
2021-09-29 15:39:11 +0700

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,6 +1,6 @@
 """
 VideoPlayerBot, Telegram Video Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+Copyright (c) 2021  klimanam <https://github.com/arullk5566>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
