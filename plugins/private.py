a2af01356e3dbf744e05ddfe1045736093f7d627
Sourcery AI <>
2022-01-07 10:23:28 +0000

    'Refactored by Sourcery'

--- plugins/private.py
+++ plugins/private.py
@@ -28,21 +28,26 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 @Client.on_message(filters.command(["start", f"start@{USERNAME}"]))
 async def start(client, message):
    buttons = [
-            [
-                InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
-            ],
-            [
-                InlineKeyboardButton("📢 CHANNEL", url=f"https://t.me/{UPDATES_CHANNEL}"),
-                InlineKeyboardButton("SOURCE 📦", url=f"https://github.com/AsmSafone/VideoPlayerBot"),
-            ],
-            [
-                InlineKeyboardButton("🤖 ABOUT", callback_data="about"),
-                InlineKeyboardButton("CLOSE 🔒", callback_data="close"),
-            ],
-            [
-               InlineKeyboardButton("➕ ADD ME TO YOUR GROUP ➕", url=f"https://t.me/{USERNAME}?startgroup=true"),
-            ]
-            ]
+       [
+           InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
+       ],
+       [
+           InlineKeyboardButton(
+               "📢 CHANNEL", url=f"https://t.me/{UPDATES_CHANNEL}"),
+           InlineKeyboardButton(
+               "SOURCE 📦", url='https://github.com/AsmSafone/VideoPlayerBot'),
+       ],
+       [
+           InlineKeyboardButton("🤖 ABOUT", callback_data="about"),
+           InlineKeyboardButton("CLOSE 🔒", callback_data="close"),
+       ],
+       [
+           InlineKeyboardButton(
+               "➕ ADD ME TO YOUR GROUP ➕",
+               url=f"https://t.me/{USERNAME}?startgroup=true",
+           ),
+       ],
+   ]
    reply_markup = InlineKeyboardMarkup(buttons)
    if message.chat.type == 'private':
        await message.reply_text(
@@ -54,68 +59,74 @@ async def start(client, message):
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
-    if query.data=="help":
-        buttons = [
-            [
-                InlineKeyboardButton("🔙 BACK", callback_data="start"),
-                InlineKeyboardButton ("SUPPORT 💬", url=f"https://t.me/{SUPPORT_GROUP}"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                HELP_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
+   if query.data=="help":
+      buttons = [
+          [
+              InlineKeyboardButton("🔙 BACK", callback_data="start"),
+              InlineKeyboardButton ("SUPPORT 💬", url=f"https://t.me/{SUPPORT_GROUP}"),
+          ]
+          ]
+      reply_markup = InlineKeyboardMarkup(buttons)
+      try:
+          await query.edit_message_text(
+              HELP_TEXT,
+              reply_markup=reply_markup
+          )
+      except MessageNotModified:
+          pass
 
-    elif query.data=="about":
-        buttons = [
-            [
-                InlineKeyboardButton("🔙 BACK", callback_data="start"),
-                InlineKeyboardButton ("SUPPORT 💬", url=f"https://t.me/{SUPPORT_GROUP}"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                ABOUT_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
+   elif query.data=="about":
+       buttons = [
+           [
+               InlineKeyboardButton("🔙 BACK", callback_data="start"),
+               InlineKeyboardButton ("SUPPORT 💬", url=f"https://t.me/{SUPPORT_GROUP}"),
+           ]
+           ]
+       reply_markup = InlineKeyboardMarkup(buttons)
+       try:
+           await query.edit_message_text(
+               ABOUT_TEXT,
+               reply_markup=reply_markup
+           )
+       except MessageNotModified:
+           pass
 
-    elif query.data=="start":
-        buttons = [
-            [
-                InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
-            ],
-            [
-                InlineKeyboardButton("📢 CHANNEL", url=f"https://t.me/TheCreatorPavan"),
-                InlineKeyboardButton("SOURCE 📦", url=f"https://github.com/AsmSafone/VideoPlayerBot"),
-            ],
-            [
-                InlineKeyboardButton("🤖 ABOUT", callback_data="about"),
-                InlineKeyboardButton("CLOSE 🔒", callback_data="close"),
-            ],
-            [
-               InlineKeyboardButton("➕ ADD ME TO YOUR GROUP ➕", url=f"https://t.me/{USERNAME}?startgroup=true"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                START_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
+   elif query.data=="start":
+      buttons = [
+          [
+              InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
+          ],
+          [
+              InlineKeyboardButton(
+                  "📢 CHANNEL", url='https://t.me/TheCreatorPavan'),
+              InlineKeyboardButton(
+                  "SOURCE 📦",
+                  url='https://github.com/AsmSafone/VideoPlayerBot'),
+          ],
+          [
+              InlineKeyboardButton("🤖 ABOUT", callback_data="about"),
+              InlineKeyboardButton("CLOSE 🔒", callback_data="close"),
+          ],
+          [
+              InlineKeyboardButton(
+                  "➕ ADD ME TO YOUR GROUP ➕",
+                  url=f"https://t.me/{USERNAME}?startgroup=true",
+              ),
+          ],
+      ]
+      reply_markup = InlineKeyboardMarkup(buttons)
+      try:
+          await query.edit_message_text(
+              START_TEXT,
+              reply_markup=reply_markup
+          )
+      except MessageNotModified:
+          pass
 
-    elif query.data=="close":
-        try:
-            await query.message.delete()
-            await query.message.reply_to_message.delete()
-        except:
-            pass
+   elif query.data=="close":
+       try:
+           await query.message.delete()
+           await query.message.reply_to_message.delete()
+       except:
+           pass
 
