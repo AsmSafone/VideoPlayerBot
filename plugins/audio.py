53baa4c1c635cac7c98decace15461848a64cfe9
RaviVeyi <112951109+RaviVeyi@users.noreply.github.com>
2022-09-12 18:38:48 +0400

    Update audio.py

--- plugins/audio.py
+++ plugins/audio.py
@@ -157,6 +157,6 @@ async def restart(client, m: Message):
     await sleep(3)
     os.execl(sys.executable, sys.executable, *sys.argv)
     try:
-        await k.edit("✅ **Restarted Successfully! \nJoin @AsmSafone For More!**")
+        await k.edit("✅ **Restarted Successfully! \nJoin @BenimHacker For More!**")
     except:
         pass
